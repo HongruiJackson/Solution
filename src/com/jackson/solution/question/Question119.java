d2d1c8f4ce999b835365da2a03ed3caba620d87b
JacksonZHR <1024060689@qq.com>
2024-06-05 21:58:08 +0800

    feat: 118. 杨辉三角

---
+++ src/com/jackson/solution/question/Question119.java
@@ -0,0 +1,26 @@
+package com.jackson.solution.question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 118. 杨辉三角
+ * https://leetcode.cn/problems/pascals-triangle/description/
+ */
+public class Question119 {
+
+    public List<List<Integer>> generate(int numRows) {
+        ArrayList<List<Integer>> wholeList = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            ArrayList<Integer> integerArrayList = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+                if (j==0) integerArrayList.add(1);
+                else integerArrayList.add(wholeList.get(i-1).get(j)+wholeList.get(i-1).get(j-1));
+            }
+            integerArrayList.add(1);
+            wholeList.add(integerArrayList);
+        }
+
+        return wholeList;
+    }
+}
